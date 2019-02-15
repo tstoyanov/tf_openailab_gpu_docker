df028896fcf856e9e525eef3e0a9095eb37022a4
Todor Stoyanov <todor.stoyanov@oru.se>
2019-02-15 13:43:39 +0100

    making startup script work outside

--- startup.sh
+++ startup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 NOTEBOOKS_PATH=/home/tsv/notebooks
-nvidia-docker run -d --name jupyter -v $NOTEBOOKS_PATH:/notebooks -e NB_UID=1000 -e NB_GID=1000 -e VNC_SERVER_PASSWORD=password -p 8888:8888 -p 6006:6006 -p 5900:5900  tstoyanov/tf-openailab-gpu 
+nvidia-docker run -d --name jupyter -v $NOTEBOOKS_PATH:/notebooks -e NB_UID=1000 -e NB_GID=1000 -e VNC_SERVER_PASSWORD=password -p 8888:8888 -p 6006:6006 -p 5900:5900  tstoyanov/tf-test:jupyter-gpu 
 echo "build succeeded, wait a bit for X to start, then getting the jupyter notebook URL with token"
 sleep 3
 docker exec -it jupyter bash -c "jupyter notebook list"
